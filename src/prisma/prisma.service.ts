c3ec1430f23970cdd47d3d9dff551769af50d54b
rayoneitalo <rayoneitalo@gmail.com>
2024-05-21 04:16:32 +0000

    chore: update prisma service init

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,15 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+    } catch (error) {
+      await this.$disconnect();
+
+      throw new BadRequestException('Erro de conexão ao banco de dados');
+    }
   }
 }
