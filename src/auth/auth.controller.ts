cc27036da0ab76162a1d41284e33ec901ee88ae2
rayoneitalo <rayoneitalo@gmail.com>
2024-05-24 20:59:11 +0000

    feat: add active account route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,6 @@ import { AuthRegisterCommand } from './commands/auth-register.command';
 import { AuthSignInCommand } from './commands/auth-sign-in.command';
 import { AuthRecoveryPasswordRequest } from './requests/auth-recovery-password.request';
 import { AuthResetPasswordRequest } from './requests/auth-reset-password.request';
-import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
